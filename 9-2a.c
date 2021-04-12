f6b4da3403fd3d31da22c084a715d51360fb0028
user <artemkalugin01@gmail.com>
2021-04-12 20:57:05 +0300

    Deleted unused files, fixes in task2

--- 9-2a.c
+++ 9-2a.c
@@ -1,92 +1,92 @@
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/sem.h>
-#include <sys/shm.h>
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/types.h>
 
-int P(int semid, struct sembuf *buf) {
-    buf->sem_op = -1;
-    buf->sem_flg = 0;
-    buf->sem_num = 0;
-
-    return semop(semid, buf, 1);
-}
-
-int V(int semid, struct sembuf *buf) {
-    buf->sem_op = 1;
-    buf->sem_flg = 0;
-    buf->sem_num = 0;
-
-    return semop(semid, buf, 1);
-}
-
-int main() {
+int main()
+{
+    int* arr;
+    long i;
     int semid;
-
-    struct sembuf mybuf;
-
-    int *array; 
-
     int shmid;
-
-    int new = 1; 
-
-    char pathname[] = "9-2a.c";
-
+    int n = 1;
+    char path[] = "9-2a.c";
     key_t key;
-
-    if ((key = ftok(pathname, 0)) < 0) {
+    struct sembuf mybuf;
+    if ((key = ftok(path, 0)) < 0) {
         printf("Cannot generate key\n");
         return -1;
     }
-
     if ((shmid = shmget(key, 3 * sizeof(int), 0666 | IPC_CREAT | IPC_EXCL)) < 0) {
         if (errno != EEXIST) {
-            printf("cannot create shared memory\n");
+            printf("Cannot create shared memory\n");
             return -1;
-        } else {
+        }
+        else {
             if ((shmid = shmget(key, 3 * sizeof(int), 0)) < 0) {
                 printf("Cannot find shared memory\n");
                 return -1;
             }
-            new = 0;
+            n = 0;
         }
     }
-
-    if ((array = (int *)shmat(shmid, NULL, 0)) == (int *)(-1)) {
-        printf("Cannot attach shared memory\n");
-        return -1;
-    }
-
-    if ((semid = semget(key, 1, 0666)) < 0) {
-        printf("Semaphore not found. Trying to create...\n");
-        if ((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0) {
-            printf("Cannot get semid\n");
+    if ((semid = semget(key, 1, 0666 | IPC_CREAT | IPC_EXCL)) < 0) {
+        if (errno != EEXIST) {
+            printf("Semaphore does not exist\n");
             return -1;
         }
-        printf("Create successful!\n");
-        V(semid, &mybuf);
+        else {
+            if ((semid = semget(key, 1, 0)) < 0) {
+                printf("Unable to get semaphore by key\n");
+                return -1;
+            }
+        }
     }
-
-    if (new) {
-        array[0] = 1;
-        array[1] = 0;
-        array[2] = 1;
-    } else {
-        P(semid, &mybuf);
-        array[0] += 1;
-        for (long i = 0; i < 1000000000L; i++);
-        array[2] += 1;
-        V(semid, &mybuf);
+    else {
+        mybuf.sem_num = 0;
+        mybuf.sem_flg = 0;
+        mybuf.sem_op = 1;
+        if (semop(semid, &mybuf, 1) < 0) {
+            printf("Cannot set original value of this semaphore to 1\n");
+            return -1;
+        }
     }
-
-    printf("Program 1 was spawn %d times, program 2 - %d times, total - %d times\n",
-           array[0], array[1], array[2]);
-
-    if (shmdt(array) < 0) {
-        printf("Cannot detach shared memory\n");
+    if ((int*)(-1) == (arr = (int*)shmat(shmid, NULL, 0))) {
+        printf("Cannot connect shared memory \n");
+        return -1;
+    }
+    mybuf.sem_num = 0;
+    mybuf.sem_flg = 0;
+    mybuf.sem_op = -1;
+    if (semop(semid, &mybuf, 1) < 0) {
+        printf("Cannot enter the critical section \n");
+        return -1;
+    }
+    if (n) {
+        arr[0] = 1;
+        arr[1] = 0;
+        arr[2] = 1;
+    }
+    else {
+        arr[0] += 1;
+        for (i = 0; i < 2000000000L; i++);
+        arr[2] += 1;
+    }
+    printf
+    ("First program was called %d times, second program - %d times, total - %d times\n",
+        arr[0], arr[1], arr[2]);
+    mybuf.sem_num = 0;
+    mybuf.sem_flg = 0;
+    mybuf.sem_op = 1;
+    if (semop(semid, &mybuf, 1) < 0) {
+        printf("Unable to exit correctlyA\n");
+        return -1;
+    }
+    if (shmdt(arr) < 0) {
+        printf("Unable to detach shared memory\n");
         return -1;
     }
     return 0;
--- 9-2b.c
+++ 9-2b.c
@@ -1,91 +1,92 @@
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/sem.h>
-#include <sys/shm.h>
 #include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/types.h>
 
-int P(int semid, struct sembuf *buf) {
-    buf->sem_op = -1;
-    buf->sem_flg = 0;
-    buf->sem_num = 0;
-
-    return semop(semid, buf, 1);
-}
-
-int V(int semid, struct sembuf *buf) {
-    buf->sem_op = 1;
-    buf->sem_flg = 0;
-    buf->sem_num = 0;
-
-    return semop(semid, buf, 1);
-}
-
-int main() {
-    int semid;
-
-    struct sembuf mybuf;
-
-    int *array; 
+int main()
+{
+    int* arr;
+    long i;
     int shmid;
-
-    int new = 1;
-
-    char pathname[] = "9-2a.c";
-
+    int semid;
+    int n = 1;
     key_t key;
-
-    if ((key = ftok(pathname, 0)) < 0) {
+    struct sembuf buf;
+    char path[] = "9-2a.c";
+    if ((key = ftok(path, 0)) < 0) {
         printf("Cannot generate key\n");
         return -1;
     }
-
     if ((shmid = shmget(key, 3 * sizeof(int), 0666 | IPC_CREAT | IPC_EXCL)) < 0) {
         if (errno != EEXIST) {
             printf("cannot create shared memory\n");
             return -1;
-        } else {
+        }
+        else {
             if ((shmid = shmget(key, 3 * sizeof(int), 0)) < 0) {
-                printf("cannot find shared memory\n");
+                printf("Cannot find shared memory\n");
                 return -1;
             }
-            new = 0;
+            n = 0;
         }
     }
-
-    if ((array = (int *)shmat(shmid, NULL, 0)) == (int *)(-1)) {
-        printf("Cannot attach shared memory\n");
-        return -1;
-    }
-
-    if ((semid = semget(key, 1, 0666)) < 0) {
-        printf("Semaphore not found. Trying to create\n");
-        if ((semid = semget(key, 1, 0666 | IPC_CREAT)) < 0) {
-            printf("Can\'t get semid\n");
+    if ((semid = semget(key, 1, 0666 | IPC_CREAT | IPC_EXCL)) < 0) {
+        if (errno != EEXIST) {
+            printf("Semaphore does not exist.\n");
             return -1;
         }
-        printf("Create successful!\n");
-        V(semid, &mybuf);
+        else {
+            if ((semid = semget(key, 1, 0)) < 0) {
+                printf("cannot get semaphore by key\n");
+                return -1;
+            }
+        }
     }
-
-    if (new) {
-        array[0] = 0;
-        array[1] = 1;
-        array[2] = 1;
-    } else {
-        P(semid, &mybuf);
-        array[1] += 1;
-        for (long i = 0; i < 1000000000L; i++);
-        array[2] += 1;
-        V(semid, &mybuf);
+    else {
+        buf.sem_num = 0;
+        buf.sem_flg = 0;
+        buf.sem_op = 1;
+        if (semop(semid, &buf, 1) < 0) {
+            printf("Cannot initialize this semaphore to 1\n");
+            return -1;
+        }
     }
-
-    printf("Program 1 was spawn %d times, program 2 - %d times, total - %d times\n",
-           array[0], array[1], array[2]);
-
-    if (shmdt(array) < 0) {
-        printf("cannot detach shared memory\n");
+    if ((int*)(-1) == (arr = (int*)shmat(shmid, NULL, 0))) {
+        printf("Cannot connect to shared memory in program B\n");
+        return -1;
+    }
+    buf.sem_num = 0;
+    buf.sem_flg = 0;
+    buf.sem_op = -1;
+    if (semop(semid, &buf, 1) < 0) {
+        printf("Cannot enter the critical section\n");
+        return -1;
+    }
+    if (n) {
+        arr[0] = 0;
+        arr[1] = 1;
+        arr[2] = 1;
+    }
+    else {
+        arr[1] += 1;
+        for (i = 0; i < 2000000000L; i++);
+        arr[2] += 1;
+    }
+    printf
+    ("First program was called %d times, second program - %d times, total - %d times\n",
+        arr[0], arr[1], arr[2]);
+    buf.sem_num = 0;
+    buf.sem_flg = 0;
+    buf.sem_op = 1;
+    if (semop(semid, &buf, 1) < 0) {
+        printf("Unable to exit the critical section correctly\n");
+        return -1;
+    }
+    if (shmdt(arr) < 0) {
+        printf("Unable to detach shared memory\n");
         return -1;
     }
     return 0;
